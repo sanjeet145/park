772156b237749d2e81f4d5ab403e8c8cd6035b62
sanjee145 <sanjeetverma9572@gmail.com>
2023-12-28 16:59:03 +0530

    small-nav

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const dropdownButton = document.querySelector(".menuBtn");
+function menubtn() {
+    navigationList.classList.toggle("show");
+}
+function bar(x) {
+    x.classList.toggle("cross");
+}
